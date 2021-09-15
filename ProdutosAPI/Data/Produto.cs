357493130293f58f819b6e471392efa03b98ceb3
Diego <contato.digpos@gmail.com>
2021-09-15 15:00:18 -0300

    Project

---
+++ ProdutosAPI/Data/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProdutosAPI.Data
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public int Estoque { get; set; }
+    }
+}
